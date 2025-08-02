9d78905346954394e204344ef3f6b79b24136370
daedsky <daedsky@tutanota.com>
2025-08-02 20:37:10 +0545

    added back gesture functionality (squashed) (master)

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import flet as ft
 from app import CrafterApp
-from routes import route_change
+from routes import route_change, view_pop
 
 
 def set_window_size(page: ft.Page):
@@ -15,6 +15,7 @@ def main(page: ft.Page):
     page.overlay.append(crafter_app.file_picker)
     page.overlay.append(crafter_app.perms_handler)
     page.on_route_change = lambda route: route_change(page=page, app=crafter_app, route=route)
+    page.on_view_pop = lambda view: view_pop(page=page, view=view)
     crafter_app.show_disclaimer_if_not_accepted()
 
 
