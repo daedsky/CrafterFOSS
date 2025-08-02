8ea882120d9cc301dfa1ff48ac50f205cbaaedb4
daedsky <daedsky@tutanota.com>
2025-08-02 23:31:34 +0545

    added nonfoss release url to install dialog

--- src/controllers/install_handler.py
+++ src/controllers/install_handler.py
@@ -1,4 +1,5 @@
 from components import custom_controls as cc
+import flet as ft
 
 # type hinting <start>
 from typing import TYPE_CHECKING
@@ -10,6 +11,14 @@ if TYPE_CHECKING:
 # type hinting <end>
 
 def install(install_layout: 'InstallLayout') -> None:
-    cc.InfoAlertDialog(page=install_layout.page,
-                       content_text='This is the FOSS version of the app. Please install PlayStore version of the the app to use this feature.',
-                       title_text='FOSS').show()
+    dlg = cc.InfoAlertDialog(page=install_layout.page,
+                             content_text='',
+                             title_text='FOSS')
+    dlg.content = ft.Text(spans=[
+        ft.TextSpan('This is the FOSS version of the app. Please install Non FOSS version of the the app '),
+        ft.TextSpan('from here', url='https://github.com/daedsky/Crafter/releases',
+                    style=ft.TextStyle(decoration=ft.TextDecoration.UNDERLINE, color=ft.Colors.BLUE,
+                                       decoration_color=ft.Colors.BLUE)),
+        ft.TextSpan(' to use this feature.')
+    ])
+    dlg.show()
