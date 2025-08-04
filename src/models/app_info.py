6ff396e2ee37bea1da9da81f08defcfb5e80f6a2
daedsky <daedsky@tutanota.com>
2025-08-04 19:01:07 +0545

    v0.0.4-beta

--- src/models/app_info.py
+++ src/models/app_info.py
@@ -2,7 +2,7 @@ from enum import StrEnum
 
 
 class AppInfo(StrEnum):
-    VERSION = '0.0.3'
+    VERSION = '0.0.4-beta'
     THEME_MODE_KEY = 'io.crafter.CrafterFOSS.ThemeMode'
     THEME_COLOR_KEY = 'io.crafter.CrafterFOSS.ThemeColor'
     LICENSE_AGREED_KEY = 'io.crafter.CrafterFOSS.LICENSE_AGREED'
